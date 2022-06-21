597a189ad4524ac7a845b9903ec4c8975a25979d
Macaspac_Samantha_Louise <96181848+MacaspacSamanthaLouise@users.noreply.github.com>
2022-06-21 23:51:47 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(e.target.email.value);
+
+    if (!e.target.email.value) {
+      alert("Email is required");
+    } else if (!e.target.email.value) {
+      alert("Valid email is required");
+    } else if (!e.target.password.value) {
+      alert("Password is required");
+    } else if (
+      e.target.email.value === "samfaucarlo@example.com" &&
+      e.target.password.value === "123456"
+    ) {
+      alert("Successfully logged in");
+      e.target.email.value = "";
+      e.target.password.value = "";
+    } else {
+      alert("Wrong email or password combination");
+    }
+  };
+
+  handleClick = e => {
+    e.preventDefault();
+
+    alert("Goes to registration page");
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <form className="form" onSubmit={this.handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" placeholder="email" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" />
+          </div>
+          <button className="primary">ENTER</button>
+        </form>
+        <button className="secondary" onClick={this.handleClick}>
+          REGISTRATION
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
